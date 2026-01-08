e6d71b9ae04a60cbad2b3808998aa7d8cec061f3
kir943 <kiransamjadhav@gmail.com>
2026-01-09 03:31:50 +0530

    Add product routes in app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,17 @@
 const express=require("express");
 const cors=require("cors");
+const connectDB=require("./config/db");
+require("dotenv").config();
+
+const productRoutes=require("./routes/product.routes");
 const app=express();
+
+connectDB();
+
 app.use(cors());
 app.use(express.json());
+//test route 
+app.use("/api/products",productRoutes);
 app.get("/",(req,res)=>{
     res.send("Sustainavle shopping assiatant Api is running");
 });
