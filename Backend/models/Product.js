105a40be07263ae9d7ebef1200763e3d24f0bab8
kir943 <kiransamjadhav@gmail.com>
2026-01-09 03:35:06 +0530

    Fix error

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -1,30 +1,32 @@
-const mongoose=require("mongoose");
-const productschema=new mongoose.Schema(
-    {
-        name:{
-            type:String,
-            required: true,
-            trim: true,
-        },
-        brand: {
-            type: String,
-            trim: true,
-        },
-        category:{
-            type: String,
-            trim: true,
-        },
-        ecoScore: {
-            type: Number,
-            min: 0,
-            max: 100,
-            default: 50,
-        },
-        isEcofriendly:{
-            type: Boolean,
-            default: false,
-        },
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {timestamps: true}
+    brand: {
+      type: String,
+      trim: true,
+    },
+    category: {
+      type: String,
+      trim: true,
+    },
+    ecoScore: {
+      type: Number,
+      min: 0,
+      max: 100,
+      default: 50,
+    },
+    isEcoFriendly: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
-module.exports =mongoose.model("Product",productSchema);
+
+module.exports = mongoose.model("Product", productSchema);
